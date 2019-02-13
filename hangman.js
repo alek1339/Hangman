dfd84d50c825e01d206e12ed27b4817044ccf4da
Aleksandar <aleksandermihaylov@gmail.com>
2019-02-13 13:11:57 +0200

    first commit

--- hangman.js
+++ hangman.js
@@ -28,7 +28,7 @@ word.innerHTML = wordInArr.join(' ')
 var errors = 0
 var score = 0
 
-function nextMove() {
+function nextMove () {
   let textInput = document.getElementById('textInput').value
 
   if (country.indexOf(textInput) !== -1 || country.indexOf(textInput.toUpperCase()) !== -1) {
@@ -71,13 +71,15 @@ function nextMove() {
   document.getElementById('textInput').value = ''
 }
 
-function newGame() {
+function newGame () {
   randomNum = parseInt(Math.random() * 10)
   country = countries[randomNum]
   score = 0
   errors = 0
   document.getElementById('textInput').disabled = false
   wordInArr = []
+  result.style.color = 'black'
+
   document.getElementById('img').src = `./images/err${errors}.gif`
   for (let i = 0; i < country.length; i++) {
     wordInArr.push('_')
